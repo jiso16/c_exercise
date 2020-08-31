7e5924588e9418857d81abd47ac18affe505ac60
sohee <totns6543@naver.com>
2020-08-31 18:21:33 +0900

    멀티스레딩 구구단

--- thread_solution/project1/pthread.c
+++ thread_solution/project1/pthread.c
@@ -6,39 +6,41 @@
 #include <pthread.h>
 #include <Windows.h>
 
+int num = 0;
 
+void* multiplication(void* data) {
 
-void* doFoo(void* data) {
-
-    int t_id = *((int*)data);
+    char* t_id = (char*)data;
+    //t_id는 data가 가르키는 주소를 가르킴
+    //디버거로 찍어봤을 때 data 주소 0x0093f704, t_id 주소 0x0093f704 value는 "First "
 
     for (int i = 0; i < 10; i++) {
-        printf("thread id(%d) : %d\n", t_id, i);
+        printf("%s : %d x %d = %d\n", t_id,num, i, num*i);
         Sleep(500);
     }
-
     return NULL;
 }
 
 
 int main() {
+    
+    printf("Enter a number to multipliaction: ");
+
+    scanf("%d", &num);
 
     pthread_t thread[3];
 
     int joinStatus;
-    int f1 = 1;
-    int f2 = 2;
-    int f3 = 3;
+    char f1[] = "First  ";
+    char f2[] = "Second ";
+    char f3[] = "Third  ";
 
-    pthread_create(&thread[0], NULL, doFoo, (void*)&f1); //스레드를 생성하고 작동시킨다
-    pthread_create(&thread[1], NULL, doFoo, (void*)&f2);
-    pthread_create(&thread[2], NULL, doFoo, (void*)&f3);
+    pthread_create(&thread[0], NULL, multiplication, (void*)&f1); //스레드를 생성하고 작동시킨다
+    pthread_create(&thread[1], NULL, multiplication, (void*)&f2);
+    pthread_create(&thread[2], NULL, multiplication, (void*)&f3);
 
     pthread_join(thread[0], (void**)&joinStatus); //스레드가 끝날때까지 기다린다
     pthread_join(thread[1], (void**)&joinStatus);
     pthread_join(thread[2], (void**)&joinStatus);
-
-    printf("Main End!");
-
+   
 }
-
